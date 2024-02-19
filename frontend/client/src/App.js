b92cd6a42e4616a6478a8f74b347e5c8515818d6
TheGodwinEbogu <godwinebogu@gmail.com>
2024-02-19 15:34:22 +0100

    BeansRite Template first modification

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,24 +1,33 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./users/BeansRiteTemplate/src/layout";
+import Combo from "./users/BeansRiteTemplate/src/pages/combo";
+import LoginPage from "./users/BeansRiteTemplate/src/components/loginPage";
+import RegisterPage from "./users/BeansRiteTemplate/src/components/registerPage";
+import { UserContextProvider } from "./users/BeansRiteTemplate/src/userContext";
+import Homepage from "./users/BeansRiteTemplate/src/Homepage";
+import Cart from "./users/BeansRiteTemplate/src/pages/cart";
+import Services from "./users/BeansRiteTemplate/src/pages/Services";
+import SpecialOffer from "./users/BeansRiteTemplate/src/pages/SpecialOffer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Homepage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/specialoffers" element={<SpecialOffer />} />
+            <Route path="/api/getcombos" element={<Combo />} />
+          </Route>
+        </Routes>
+      </Router>
+    </UserContextProvider>
   );
-}
+};
 
 export default App;
