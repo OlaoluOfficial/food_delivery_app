30ad97aad79c7daf372de6a74d3a7637493297bf
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2024-02-24 09:33:36 +0100

    Add cart

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/food_delivery_app');
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
