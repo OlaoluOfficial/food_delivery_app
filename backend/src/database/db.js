1adc83182a2f0cd939dda174c1ab9170721b7900
TheGodwinEbogu <godwinebogu@gmail.com>
2024-02-26 21:22:50 +0100

    backend commit

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/your-database-name', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('Connected to MongoDB');
+    await mongoose.connect(
+      "mongodb://localhost:27017/your-database-namemongodb+srv://k2law:lawlund%40k2law@cluster0.zkjwxrb.mongodb.net/FoodDeliveryService",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
+    console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
   }
 };
