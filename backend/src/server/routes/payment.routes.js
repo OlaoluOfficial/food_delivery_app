532119da7f806303716a8e2ea02d91e563114364
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2024-03-13 19:37:05 +0100

    work on payments

--- backend/src/server/routes/payment.routes.js
+++ backend/src/server/routes/payment.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const AuthMiddleware = require('../middlewares/auth.middleware');
 const PaymentController = require('../controllers/payment.controller');
 
-router.post('/', PaymentController.charge);
+router.post('/', AuthMiddleware.authenticateUser, PaymentController.charge);
 router.post('/webhook', PaymentController.handleWebhook);
+router.post('/complete-payment', PaymentController.paymentComplete);
 
 module.exports = router;
