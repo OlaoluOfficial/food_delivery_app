cc7ce7ec46d68d285686e97037db29aa13ca85c0
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2024-03-17 01:28:41 +0100

    fix search

--- backend/src/server/controllers/search.controller.js
+++ backend/src/server/controllers/search.controller.js
@@ -1,4 +1,3 @@
-const Restaurant = require('../../models/restaurant');
 const Product = require('../../models/product');
 
 class SearchController {
@@ -6,10 +5,9 @@ class SearchController {
     try {
       const query = req.query.q;
 
-      const restaurantResults = await Restaurant.find({ name: { $regex: query, $options: 'i' } });
       const productResults = await Product.find({ name: { $regex: query, $options: 'i' } });
   
-      res.status(200).json({ restaurants: restaurantResults, products: productResults });
+      res.status(200).json({ products: productResults });
     } catch (error) {
       console.error('Error searching:', error);
       res.status(500).json({ message: 'Internal server error' });
